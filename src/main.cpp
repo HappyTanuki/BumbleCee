51a49a4470ebabc03de1d92fc60eb022a7370c65
HappyTanuki <rlaalstn120@icloud.com>
2025-08-29 05:44:56 +0900

    ffmpeg 부를 때 무조건 extern c 하기...

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <boost/process.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/ssl.hpp>
+extern "C" {
 #include <libavformat/avformat.h>
+}
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+    std::cout << "FFmpeg version: " << av_version_info() << std::endl;
     return 0;
 }
